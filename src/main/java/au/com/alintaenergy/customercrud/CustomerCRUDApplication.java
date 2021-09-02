6955e5e926fe8df54438079cfd1545997ddfe2c8
Stuart Parker <2979257+stupar73@users.noreply.github.com>
2021-09-02 10:52:15 +1000

    Project skeleton
    Generated using Spring Initializr (https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.5.4&packaging=jar&jvmVersion=16&groupId=au.com.alintaenergy.customercrud&artifactId=alintaenergy-codetest-customercrud&name=Alinta%20Energy%20code%20test%20-%20Customer%20CRUD&description=Customer%20CRUD%20application%20for%20Alinta%20Energy%20coding%20test&packageName=au.com.alintaenergy.customercrud&dependencies=web%2ch2%2cdata-jpa)

---
+++ src/main/java/au/com/alintaenergy/customercrud/CustomerCRUDApplication.java
@@ -0,0 +1,13 @@
+package au.com.alintaenergy.customercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCRUDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerCRUDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/au/com/alintaenergy/customercrud/CustomerCRUDApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.alintaenergy.customercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCRUDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
