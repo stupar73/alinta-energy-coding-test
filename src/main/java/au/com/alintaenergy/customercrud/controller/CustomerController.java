87d58c452e9c6f5625be90bf56319539b8204314
Stuart Parker <2979257+stupar73@users.noreply.github.com>
2021-09-02 16:07:22 +1000

    Use simpler request mapping annotations in controller

--- src/main/java/au/com/alintaenergy/customercrud/controller/CustomerController.java
+++ src/main/java/au/com/alintaenergy/customercrud/controller/CustomerController.java
@@ -5,10 +5,13 @@ import au.com.alintaenergy.customercrud.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,28 +26,28 @@ public class CustomerController extends BaseController {
     @Autowired
     CustomerBusiness customerBusiness;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Customer> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(customerBusiness.findById(id));
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Customer>> findAllByAttributes(@RequestParam("firstName") Optional<String> firstName,
             @RequestParam("lastName") Optional<String> lastName) {
         return ResponseEntity.ok(customerBusiness.findAllByAttributes(firstName, lastName));
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<Customer> create(@RequestBody @Valid Customer newCustomer) {
         return ResponseEntity.status(HttpStatus.CREATED).body(customerBusiness.create(newCustomer));
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Customer> update(@PathVariable("id") Long id, @RequestBody @Valid Customer updatedCustomer) {
-            return ResponseEntity.ok(customerBusiness.updateById(id, updatedCustomer));
+        return ResponseEntity.ok(customerBusiness.updateById(id, updatedCustomer));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         customerBusiness.delete(id);
         return ResponseEntity.noContent().build();
